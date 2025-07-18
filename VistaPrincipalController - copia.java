b057a586bdc8c55a91d577d1a0c47ec6ce2fec20
German2900 <20243ds118@utez.edu.mx>
2025-07-17 21:20:54 -0600

    vista_principal
    subo la vista principal del proyecto

---
+++ VistaPrincipalController - copia.java
@@ -0,0 +1,29 @@
+package com.example.evento;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.image.ImageView;
+import javafx.scene.layout.Pane;
+
+public class VistaPrincipalController {
+
+    @FXML private Button btnAgregar;
+
+    @FXML private Pane tarjetaPrincipal;
+    @FXML private Label lblNombrePrincipal;
+    @FXML private Label lblDescripcionPrincipal;
+    @FXML private ImageView imgPrincipal;
+
+    @FXML private Pane tarjetaMini1;
+    @FXML private Label lblNombreMini1;
+    @FXML private Label lblDescripcionMini1;
+    @FXML private ImageView imgMini1;
+
+    @FXML private Pane tarjetaMini2;
+    @FXML private Label lblNombreMini2;
+    @FXML private Label lblDescripcionMini2;
+    @FXML private ImageView imgMini2;
+
+    // Aquí puedes poner la lógica que necesites, por ejemplo, en initialize()
+}
